44b4134607b1bcb3b849fe9bd062c2fa3e9a7c0c
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2023-11-22 18:25:17 +0100

    Update Login.cs

--- osmaker/Login.cs
+++ osmaker/Login.cs
@@ -24,69 +24,9 @@ namespace osmaker
             this.Fclient = Fclient;
         }
 
-        public static string Encrypt(string value)
-        {
-
-            return Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
-
-        }
-        public static string Decrypt(string value)
-        {
-
-            return Encoding.UTF8.GetString(Convert.FromBase64String(value));
-
-        }
-
         private void guna2Button1_Click(object sender, EventArgs e)
         {
             label1.Text = "Logging in";
-            try
-            {
-                var response = Fclient.Get(guna2TextBox1.Text);
-                if (response.Body == "null")
-                {
-                    
-                    Fclient.Set($@"{guna2TextBox1.Text}/Password", Encrypt(guna2TextBox2.Text));
-                    Fclient.Set($@"{guna2TextBox1.Text}/Friends", new List<string>() {"ME"});
-                    Fclient.Set($@"{guna2TextBox1.Text}/Chats", new Dictionary<string, string>{{ "ME:0", "CHAT" }});
-                    label1.Text = "no accounts found, creating one";
-                    main_page.loggedin = true;
-                    main_page.username = guna2TextBox1.Text;
-                    main_page.password = guna2TextBox2.Text;
-                    File.WriteAllText($@"{Application.StartupPath}\OSM.login", $"{guna2TextBox1.Text}\n{guna2TextBox2.Text}");
-                    this.Close();
-                    Application.Restart();
-
-                }
-                else
-                {
-                    var a = Fclient.Get($@"{guna2TextBox1.Text}/Password");
-                    if (guna2TextBox2.Text == Decrypt(a.Body.Replace(@"""","")))
-                    {
-
-                        label1.Text = "Logged in";
-                        main_page.loggedin = true;
-                        File.WriteAllText($@"{Application.StartupPath}\OSM.login", $"{guna2TextBox1.Text}\n{Encrypt(guna2TextBox2.Text)}");
-                        Application.Restart();
-                        this.Close();
-
-                    }
-                    else 
-                    {
-
-                        label1.Text = "incorrect password";
-
-                    }
-                }
-            }
-            catch
-            {
-                MessageBox.Show("Invalid Token");
-
-            }
-
-
-            
         }
 
         private void Login_Load(object sender, EventArgs e)
